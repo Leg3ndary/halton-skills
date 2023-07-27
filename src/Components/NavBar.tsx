0e193e155fe98d80972f396fb979773d5ad3a6fc
_Leg3ndary <ben.zhou.2006@gmail.com>
2023-07-26 22:22:23 -0400

    Add more css and stuff

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./NavBar.css";
 import logo from "../assets/halton_skills_logo_only.png";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
     return (
         <div className="navbar">
             <div className="navbar_logo">
-                <a href="/">
+                <Link to="/">
                     <img src={logo} alt="Halton Skills Logo"></img>
-                </a>
+                </Link>
+            </div>
+            <div className="links">
+                <button className="navbar_button">
+                    <Link to="/contact">Contact</Link>
+                </button>
+                <Link to="/news">News</Link>
+                <Link to="/elementary-competitions">Elementary</Link>
+                <Link to="/secondary-competitions">Secondary</Link>
+                <Link to="/scopes">Scopes</Link>
+                <Link to="/sponsors-donate">Sponsors/Donate</Link>
             </div>
         </div>
     );
