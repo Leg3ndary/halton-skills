5a1e051671bff5c3cb56cf60988137f29f1796ef
Leg3ndary <ben.zhou.2006@gmail.com>
2023-10-27 08:15:15 -0400

    Push some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-full bg-slate-600">
+    <h1 class="">Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</div>
